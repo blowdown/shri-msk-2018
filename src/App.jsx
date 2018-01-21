d13af8608c44804c0a9d2453d7ff72080be9b9c9
Sergey Korotkov <serg.korotkov.91@gmail.com>
2018-01-22 02:02:27 +0300

    Merge App into Meeting, Meeting state and actions

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Button from './Button';
 import Modal from './Modal';
-import Header from './Header';
-import Footer from './Footer';
 import Meeting from './Meeting';
-import './app.css';
+
+const members = [
+    { id: 1, login: 'Лекс Лютер', homeFloor: 11, avatarUrl: 'https://randomuser.me/api/portraits/men/1.jpg' },
+    { id: 2, login: 'Дарт Вейдер', homeFloor: 7, avatarUrl: 'https://randomuser.me/api/portraits/men/2.jpg' },
+    { id: 3, login: 'Томас Андерсон', homeFloor: 3, avatarUrl: 'https://randomuser.me/api/portraits/men/3.jpg' }
+];
+
+const rooms = [
+    { id: 1, title: '404', capacity: 5, floor: 4 },
+    { id: 2, title: 'Деньги', capacity: 4, floor: 2 },
+    { id: 3, title: 'Карты', capacity: 4, floor: 2 },
+    { id: 4, title: 'Ствола', capacity: 2, floor: 2 },
+    { id: 5, title: '14', capacity: 6, floor: 3 }
+];
 
 export default class App extends React.Component {
-    constructor (props, context) {
-        super(props, context);
-    }
-
     render() {
-        return <div className="app">
-
-            <Header></Header>
-
-            <div className="app__content-scroll">
-                <div className="app__content">
-                    <Meeting></Meeting>
-                </div>
-            </div>
-
-            <Footer>
-                <Button>Отмена</Button>
-                <Button primary={true}>Создать встречу</Button>
-            </Footer>
-        </div>
+        return (
+            <Meeting
+                members={members}
+                rooms={rooms}
+            />
+        );
     }
 }
--- src/Meeting.jsx
+++ src/Meeting.jsx
@@ -3,76 +3,17 @@ import React from 'react';
 import Textbox from './Textbox';
 import Dropdown from './Dropdown';
 import MembersDropdown from './MembersDropdown';
+import Header from './Header';
+import Footer from './Footer';
+import Button from './Button';
+import { Room, SelectedRoom } from './Room';
+import * as arrayUtils from './arrayUtils';
 
+import './app.css';
 import './meeting.css';
 import './meeting__header.css';
 
 import './member.css';
-import './room.css';
-
-function arrayToggle(array, element) {
-    array = array.slice();
-    const ix = array.indexOf(element);
-    if (ix === -1) {
-        array.push(element);
-    } else {
-        array.splice(ix, 1);
-    }
-
-    return array;
-}
-
-function arrayRemove(array, element) {
-    const ix = array.indexOf(element);
-    if (ix === -1) {
-        return array;
-    }
-
-    array = array.slice();
-    if (ix !== -1) {
-        array.splice(ix, 1);
-    }
-
-    return array;
-}
-
-
-const members = [
-    { id: 1, login: 'Лекс Лютер', homeFloor: 11, avatarUrl: 'https://randomuser.me/api/portraits/men/1.jpg' },
-    { id: 2, login: 'Дарт Вейдер', homeFloor: 7, avatarUrl: 'https://randomuser.me/api/portraits/men/2.jpg' },
-    { id: 3, login: 'Томас Андерсон', homeFloor: 3, avatarUrl: 'https://randomuser.me/api/portraits/men/3.jpg' }
-];
-
-const rooms = [
-    { id: 1, title: '404', capacity: 5, floor: 4 },
-    { id: 2, title: 'Деньги', capacity: 4, floor: 2 },
-    { id: 3, title: 'Карты', capacity: 4, floor: 2 },
-    { id: 4, title: 'Ствола', capacity: 2, floor: 2 },
-    { id: 5, title: '14', capacity: 6, floor: 3 }
-];
-
-const Room = ({ timeBegin, timeEnd, room: { title, floor }, onClick }) => (
-    <div className="room" onClick={onClick}>
-        <div className="room__time">
-            {timeBegin.getHours()}:{timeBegin.getMinutes()}
-            —
-            {timeEnd.getHours()}:{timeEnd.getMinutes()}
-        </div>
-        <div className="room__name">{title} · {floor} этаж</div>
-    </div>
-);
-
-const SelectedRoom = ({ timeBegin, timeEnd, room: { title, floor }, onRemove }) => (
-    <div className="room room--selected">
-        <div className="room__time">
-            {timeBegin.getHours()}:{timeBegin.getMinutes()}
-            —
-            {timeEnd.getHours()}:{timeEnd.getMinutes()}
-        </div>
-        <div className="room__name">{title} · {floor} этаж</div>
-        <button className="room__cancel" onClick={onRemove}></button>
-    </div>
-)
 
 export default class Meeting extends React.Component {
     constructor(props, context) {
@@ -80,99 +21,141 @@ export default class Meeting extends React.Component {
 
         this.state = {
             selectedMembers: [],
-            selectedRoom: null
+            selectedRoom: null,
+            title: '',
+            date: '',
+            timeBegin: '',
+            timeEnd: ''
         };
+
+        this._cancelMeeting = this._cancelMeeting.bind(this);
+        this._createMeeting = this._createMeeting.bind(this);
+    }
+
+    _cancelMeeting() {
+        alert('cancel');
+    }
+
+    _createMeeting() {
+        alert('create ' + JSON.stringify(this.state, null, 4));
     }
 
     render() {
         return (
-            <div className="meeting">
-                <div className="meeting__header">
-                    <h1 className="meeting__header-text">Редактирование встречи</h1>
-                    <button className="meeting__header-close"></button>
-                </div>
+            <div className="app">
+                <Header />
 
-                <div className="meeting__first-row">
-                    <div className="meeting__title">
-                        <h2 className="meeting__block-title">Тема</h2>
-                        <Textbox placeholder="О чем будем говорить?" />
-                    </div>
+                <div className="app__content-scroll">
+                    <div className="app__content">
+                        <div className="meeting">
+                            <div className="meeting__header">
+                                <h1 className="meeting__header-text">Новая встреча</h1>
+                                <button className="meeting__header-close"
+                                    onClick={this._cancelMeeting}
+                                />
+                            </div>
 
-                    <div className="meeting__date">
-                        <h2 className="meeting__block-title">Дата</h2>
-                        <Textbox placeholder="13 января 2018" icon="calendar" />
-                    </div>
-
-                    <div className="meeting__start">
-                        <h2 className="meeting__block-title">Начало</h2>
-                        <Textbox placeholder="16:00" showClear={false} />
-                    </div>
-
-                    <div className="meeting__dash"></div>
-
-                    <div className="meeting__end">
-                        <h2 className="meeting__block-title">Конец</h2>
-                        <Textbox placeholder="17:00" showClear={false} />
-                    </div>
-                </div>
-
-                <div className="meeting__second-row">
-                    <div className="meeting__members">
-                        <h2 className="meeting__block-title">Участники</h2>
-
-                        <div className="meeting__members-dropdown">
-                            <MembersDropdown
-                                selected={this.state.selectedMembers}
-                                members={members}
-                                onSelect={member => {
-                                    this.setState({ selectedMembers: arrayToggle(this.state.selectedMembers, member) });
-                                }}
-                            />
-                        </div>
-
-                        {
-                            this.state.selectedMembers.map(member => (
-                                <div className="member" key={member.id}>
-                                    <div className="member__avatar" style={{ backgroundImage: `url(${member.avatarUrl})` }} />
-                                    <div className="member__name">{member.login}</div>
-                                    <button className="member__remove"
-                                        onClick={() => {
-                                            this.setState({ selectedMembers: arrayRemove(this.state.selectedMembers, member) });
-                                        }}
+                            <div className="meeting__first-row">
+                                <div className="meeting__title">
+                                    <h2 className="meeting__block-title">Тема</h2>
+                                    <Textbox
+                                        placeholder="О чем будем говорить?"
+                                        onChange={(title) => this.setState({ title: title })}
+                                        value={this.state.title}
                                     />
                                 </div>
-                            ))
-                        }
-                    </div>
 
-                    <div className="meeting__room">
-                        <h2 className="meeting__block-title">Ваша переговорка</h2>
-
-                        {this.state.selectedRoom ?
-                            (
-                                <SelectedRoom
-                                    timeBegin={new Date()}
-                                    timeEnd={new Date()}
-                                    room={this.state.selectedRoom}
-                                    key={this.state.selectedRoom.id}
-                                    onRemove={ () => this.setState({ selectedRoom: null })}
-                                />
-                            ) :
-                            (
-                                rooms.map(room => (
-                                    <Room
-                                        timeBegin={new Date()}
-                                        timeEnd={new Date()}
-                                        room={room}
-                                        key={room.id}
-                                        onClick={() => this.setState({selectedRoom: room})}
+                                <div className="meeting__date">
+                                    <h2 className="meeting__block-title">Дата</h2>
+                                    <Textbox placeholder="13 января 2018" icon="calendar"
+                                        onChange={(date) => this.setState({ date: date })}
+                                        value={this.state.date}
                                     />
-                                ))
-                            )
-                        }
+                                </div>
+
+                                <div className="meeting__start">
+                                    <h2 className="meeting__block-title">Начало</h2>
+                                    <Textbox placeholder="16:00" showClear={false}
+                                        onChange={(timeBegin) => this.setState({ timeBegin: timeBegin })}
+                                        value={this.state.timeBegin}
+                                    />
+                                </div>
+
+                                <div className="meeting__dash"></div>
+
+                                <div className="meeting__end">
+                                    <h2 className="meeting__block-title">Конец</h2>
+                                    <Textbox placeholder="17:00" showClear={false}
+                                        onChange={(timeEnd) => this.setState({ timeEnd: timeEnd })}
+                                        value={this.state.timeEnd} />
+                                </div>
+                            </div>
+
+                            <div className="meeting__second-row">
+                                <div className="meeting__members">
+                                    <h2 className="meeting__block-title">Участники</h2>
+
+                                    <div className="meeting__members-dropdown">
+                                        <MembersDropdown
+                                            selected={this.state.selectedMembers}
+                                            members={this.props.members.filter(member => this.state.selectedMembers.indexOf(member) === -1)}
+                                            onSelect={member => {
+                                                this.setState({ selectedMembers: arrayUtils.toggle(this.state.selectedMembers, member) });
+                                            }}
+                                        />
+                                    </div>
+
+                                    {
+                                        this.state.selectedMembers.map(member => (
+                                            <div className="member" key={member.id}>
+                                                <div className="member__avatar" style={{ backgroundImage: `url(${member.avatarUrl})` }} />
+                                                <div className="member__name">{member.login}</div>
+                                                <button className="member__remove"
+                                                    onClick={() => {
+                                                        this.setState({ selectedMembers: arrayUtils.remove(this.state.selectedMembers, member) });
+                                                    }}
+                                                />
+                                            </div>
+                                        ))
+                                    }
+                                </div>
+
+                                <div className="meeting__room">
+                                    <h2 className="meeting__block-title">Ваша переговорка</h2>
+
+                                    {this.state.selectedRoom ?
+                                        (
+                                            <SelectedRoom
+                                                timeBegin={new Date()}
+                                                timeEnd={new Date()}
+                                                room={this.state.selectedRoom}
+                                                key={this.state.selectedRoom.id}
+                                                onRemove={() => this.setState({ selectedRoom: null })}
+                                            />
+                                        ) :
+                                        (
+                                            this.props.rooms.map(room => (
+                                                <Room
+                                                    timeBegin={new Date()}
+                                                    timeEnd={new Date()}
+                                                    room={room}
+                                                    key={room.id}
+                                                    onClick={() => this.setState({ selectedRoom: room })}
+                                                />
+                                            ))
+                                        )
+                                    }
+                                </div>
+                            </div>
+                        </div>
 
                     </div>
                 </div>
+
+                <Footer>
+                    <Button onClick={this._cancelMeeting}>Отмена</Button>
+                    <Button primary={true} onClick={this._createMeeting}>Создать встречу</Button>
+                </Footer>
             </div>
         );
     }
